4eccc543fa91ba02bcb86ab5a30f13233d9897ec
Keith Donald <keith.donald@springsource.com>
2010-06-23 03:53:52 -0400

    consumer details service

--- src/main/java/com/springsource/greenhouse/oauth/GreenhouseTokenServices.java
+++ src/main/java/com/springsource/greenhouse/oauth/GreenhouseTokenServices.java
@@ -1,14 +1,18 @@
 package com.springsource.greenhouse.oauth;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.inject.Inject;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.security.oauth.provider.token.OAuthProviderTokenImpl;
 import org.springframework.security.oauth.provider.token.RandomValueProviderTokenServices;
 
+// we need to think through persistence here... are we sure we want to be storing oauth tokens and access tokens in the same table?
 public class GreenhouseTokenServices extends RandomValueProviderTokenServices {
 	
-	@SuppressWarnings("unused")
 	private JdbcTemplate jdbcTemplate;
 	
 	@Inject
@@ -18,19 +22,26 @@ public class GreenhouseTokenServices extends RandomValueProviderTokenServices {
 	
     @Override
     protected OAuthProviderTokenImpl readToken(String tokenValue) {
-		// TODO build token from DB using JdbcTemplate
-    	return null;
+		return this.jdbcTemplate.queryForObject("select access from UserApp where token = ?", new RowMapper<OAuthProviderTokenImpl>() {
+			public OAuthProviderTokenImpl mapRow(ResultSet rs, int rowNum) throws SQLException {
+				OAuthProviderTokenImpl token = new OAuthProviderTokenImpl();
+				token.setAccessToken(rs.getBoolean("access"));
+				return token;
+			}
+			
+		}, tokenValue);
     }
 
     @Override
     protected OAuthProviderTokenImpl removeToken(String tokenValue) {
-		// TODO remove token from DB using JdbcTemplate
+    	this.jdbcTemplate.update("delete from UserApp where token = ?", tokenValue);
     	return null;
     }
 
     @Override
     protected void storeToken(String tokenValue, OAuthProviderTokenImpl token) {
-		// TODO save token to DB using JdbcTemplate
+    	// how to associate with user if its an oauth token?
+    	this.jdbcTemplate.update("insert (token, access) into UserApp values (?, ?)", tokenValue, token.isAccessToken());    	
     }
 
 }
