f5a90e5e50eb46da9e45e0b50e7d0ad814893cd5
Keith Donald <keith.donald@springsource.com>
2010-06-23 03:41:50 -0400

    consumer details service

--- src/main/java/com/springsource/greenhouse/oauth/GreenhouseConsumerDetailsService.java
+++ src/main/java/com/springsource/greenhouse/oauth/GreenhouseConsumerDetailsService.java
@@ -1,19 +1,22 @@
 package com.springsource.greenhouse.oauth;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.inject.Inject;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth.common.OAuthException;
+import org.springframework.security.oauth.common.signature.SharedConsumerSecret;
 import org.springframework.security.oauth.provider.BaseConsumerDetails;
 import org.springframework.security.oauth.provider.ConsumerDetails;
 import org.springframework.security.oauth.provider.ConsumerDetailsService;
 
 public class GreenhouseConsumerDetailsService implements ConsumerDetailsService {
 
-	@SuppressWarnings("unused")
 	private JdbcTemplate jdbcTemplate;
 	
 	@Inject
@@ -21,13 +24,20 @@ public class GreenhouseConsumerDetailsService implements ConsumerDetailsService 
 		this.jdbcTemplate = jdbcTemplate;
 	}
 
-	public ConsumerDetails loadConsumerByConsumerKey(String key) throws OAuthException {
-		BaseConsumerDetails consumerDetails = new BaseConsumerDetails();
-		// TODO build consumer details from DB using JdbcTemplate
-        consumerDetails.setResourceName("Your Greenhouse Profile");
-        consumerDetails.setResourceDescription("Everything Greenhouse knows about you.");
-        consumerDetails.setAuthorities(new ArrayList<GrantedAuthority>());        
-		return consumerDetails;
+	public ConsumerDetails loadConsumerByConsumerKey(final String key) throws OAuthException {
+		return jdbcTemplate.queryForObject("select name, secret from App where consumerKey = ?", new RowMapper<ConsumerDetails>() {
+			public ConsumerDetails mapRow(ResultSet rs, int rowNum) throws SQLException {
+				BaseConsumerDetails consumerDetails = new BaseConsumerDetails();
+				consumerDetails.setConsumerName(rs.getString("name"));
+				consumerDetails.setConsumerKey(key);
+				consumerDetails.setSignatureSecret(new SharedConsumerSecret(rs.getString("secret")));
+				consumerDetails.setRequiredToObtainAuthenticatedToken(true);
+		        consumerDetails.setResourceName("Your Greenhouse Profile");
+		        consumerDetails.setResourceDescription("Everything Greenhouse knows about you.");
+		        consumerDetails.setAuthorities(new ArrayList<GrantedAuthority>());        
+				return consumerDetails;
+			}
+		}, key);
 	}
 
 }
