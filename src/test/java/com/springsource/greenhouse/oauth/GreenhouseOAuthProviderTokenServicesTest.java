9ff9bebcd25ee52f12796baa426f7818617b98d0
habuma <cwalls@vmware.com>
2010-06-25 13:55:33 -0500

    Fix for re-authorization problem

--- src/test/java/com/springsource/greenhouse/oauth/GreenhouseOAuthProviderTokenServicesTest.java
+++ src/test/java/com/springsource/greenhouse/oauth/GreenhouseOAuthProviderTokenServicesTest.java
@@ -79,4 +79,102 @@ public class GreenhouseOAuthProviderTokenServicesTest {
     	assertEquals("doesnt-matter-yet", accessToken2.getUserAuthentication().getCredentials());
     }
     
+    @Test
+    public void shouldReauthorize() {
+        // TODO - This test duplicates a large portion of the previous test and needs to be cleaned up. But
+        //        I needed to commit this to get the fix under test and wanted to be sure it was in the 
+        //        code repo before I left for my flight. I'll certainly revisit this to remove duplication
+        //        later.
+        OAuthProviderToken token = tokenServices.createUnauthorizedRequestToken("a08318eb478a1ee31f69a55276f3af64", "x-com-springsource-greenhouse://oauth-response");
+        assertFalse(token.isAccessToken());
+        assertNotNull(token.getValue());
+        assertEquals("a08318eb478a1ee31f69a55276f3af64", token.getConsumerKey());
+        assertNotNull(token.getSecret());
+        assertEquals("x-com-springsource-greenhouse://oauth-response", token.getCallbackUrl());
+        assertNull(token.getVerifier());
+        
+        OAuthProviderToken token2 = tokenServices.getToken(token.getValue());
+        assertEquals(token.isAccessToken(), token2.isAccessToken());
+        assertEquals(token.getValue(), token2.getValue());
+        assertEquals(token.getConsumerKey(), token2.getConsumerKey());
+        assertEquals(token.getSecret(), token2.getSecret());
+        assertEquals(token.getCallbackUrl(), token2.getCallbackUrl());
+        assertEquals(token.getVerifier(), token.getVerifier());
+        
+        Authentication auth = new TestingAuthenticationToken(new GreenhouseUserDetails(1L, "rclarkson@vmware.com", "rclarkson", "Roy"), "atlanta");
+        tokenServices.authorizeRequestToken(token.getValue(), "12345", auth);
+
+        OAuthProviderToken token3 = tokenServices.getToken(token.getValue());
+        assertEquals(token.isAccessToken(), token3.isAccessToken());
+        assertEquals(token.getValue(), token3.getValue());
+        assertEquals(token.getConsumerKey(), token3.getConsumerKey());
+        assertEquals(token.getSecret(), token3.getSecret());
+        assertEquals(token.getCallbackUrl(), token3.getCallbackUrl());
+        assertEquals("12345", token3.getVerifier());
+
+        OAuthProviderToken accessToken = tokenServices.createAccessToken(token3.getValue());
+        assertEquals(true, accessToken.isAccessToken());
+        assertFalse(token.getValue().equals(accessToken.getValue()));
+        assertEquals(token.getConsumerKey(), accessToken.getConsumerKey());
+        assertNotNull(accessToken.getSecret());
+        assertNull(accessToken.getVerifier());
+        assertNull(accessToken.getCallbackUrl());
+        
+        OAuthAccessProviderToken accessToken2 = (OAuthAccessProviderToken) tokenServices.getToken(accessToken.getValue());
+        assertEquals(accessToken.isAccessToken(), accessToken2.isAccessToken());
+        assertEquals(accessToken.getValue(), accessToken2.getValue());
+        assertEquals(accessToken.getConsumerKey(), accessToken2.getConsumerKey());
+        assertNull(accessToken2.getSecret());
+        assertNull(accessToken2.getCallbackUrl());
+        assertNull(accessToken2.getVerifier());
+        assertNotNull(accessToken2.getUserAuthentication());
+        assertEquals("doesnt-matter-yet", accessToken2.getUserAuthentication().getPrincipal());
+        assertEquals("doesnt-matter-yet", accessToken2.getUserAuthentication().getCredentials());     
+        
+        OAuthProviderToken reAuthToken = tokenServices.createUnauthorizedRequestToken("a08318eb478a1ee31f69a55276f3af64", "x-com-springsource-greenhouse://oauth-response");
+        assertFalse(reAuthToken.isAccessToken());
+        assertNotNull(reAuthToken.getValue());
+        assertEquals("a08318eb478a1ee31f69a55276f3af64", reAuthToken.getConsumerKey());
+        assertNotNull(reAuthToken.getSecret());
+        assertEquals("x-com-springsource-greenhouse://oauth-response", reAuthToken.getCallbackUrl());
+        assertNull(reAuthToken.getVerifier());
+        
+        OAuthProviderToken reAuthToken2 = tokenServices.getToken(reAuthToken.getValue());
+        assertEquals(reAuthToken.isAccessToken(), reAuthToken2.isAccessToken());
+        assertEquals(reAuthToken.getValue(), reAuthToken2.getValue());
+        assertEquals(reAuthToken.getConsumerKey(), reAuthToken2.getConsumerKey());
+        assertEquals(reAuthToken.getSecret(), reAuthToken2.getSecret());
+        assertEquals(reAuthToken.getCallbackUrl(), reAuthToken2.getCallbackUrl());
+        assertEquals(reAuthToken.getVerifier(), reAuthToken.getVerifier());
+        
+        tokenServices.authorizeRequestToken(reAuthToken.getValue(), "12345", auth);        
+        
+        
+        OAuthProviderToken reAuthToken3 = tokenServices.getToken(reAuthToken.getValue());
+        assertEquals(reAuthToken.isAccessToken(), reAuthToken3.isAccessToken());
+        assertEquals(reAuthToken.getValue(), reAuthToken3.getValue());
+        assertEquals(reAuthToken.getConsumerKey(), reAuthToken3.getConsumerKey());
+        assertEquals(reAuthToken.getSecret(), reAuthToken3.getSecret());
+        assertEquals(reAuthToken.getCallbackUrl(), reAuthToken3.getCallbackUrl());
+        assertEquals("12345", reAuthToken3.getVerifier());
+
+        OAuthProviderToken reAuthAccessToken = tokenServices.createAccessToken(reAuthToken3.getValue());
+        assertEquals(true, reAuthAccessToken.isAccessToken());
+        assertFalse(reAuthToken.getValue().equals(reAuthAccessToken.getValue()));
+        assertEquals(reAuthToken.getConsumerKey(), reAuthAccessToken.getConsumerKey());
+        assertNotNull(reAuthAccessToken.getSecret());
+        assertNull(reAuthAccessToken.getVerifier());
+        assertNull(reAuthAccessToken.getCallbackUrl());
+        
+        OAuthAccessProviderToken reAuthAccessToken2 = (OAuthAccessProviderToken) tokenServices.getToken(reAuthAccessToken.getValue());
+        assertEquals(reAuthAccessToken.isAccessToken(), reAuthAccessToken2.isAccessToken());
+        assertEquals(reAuthAccessToken.getValue(), reAuthAccessToken2.getValue());
+        assertEquals(reAuthAccessToken.getConsumerKey(), reAuthAccessToken2.getConsumerKey());
+        assertNull(reAuthAccessToken2.getSecret());
+        assertNull(reAuthAccessToken2.getCallbackUrl());
+        assertNull(reAuthAccessToken2.getVerifier());
+        assertNotNull(reAuthAccessToken2.getUserAuthentication());
+        assertEquals("doesnt-matter-yet", reAuthAccessToken2.getUserAuthentication().getPrincipal());
+        assertEquals("doesnt-matter-yet", reAuthAccessToken2.getUserAuthentication().getCredentials());  
+    }
 }
--- src/test/resources/com/springsource/greenhouse/oauth/GreenhouseOAuthProviderTokenServicesTest.sql
+++ src/test/resources/com/springsource/greenhouse/oauth/GreenhouseOAuthProviderTokenServicesTest.sql
@@ -1,3 +1,7 @@
+drop table if exists OAuthToken;
+drop table if exists AuthorizedConsumer;
+drop table if exists Consumer;
+drop table if exists User;
 create table User (id identity,
 					firstName varchar not null, 
 					lastName varchar not null,
@@ -35,4 +39,7 @@ create table OAuthToken (tokenValue varchar not null unique,
 					foreign key (userId) references User(id));
 					
 insert into User (firstName, lastName, email, password, username) values ('Roy', 'Clarkson', 'rclarkson@vmware.com', 'atlanta', 'rclarkson');
+insert into User (firstName, lastName, email, password, username) values ('Craig', 'Walls', 'cwalls@vmware.com', 'plano', 'habuma');
 insert into Consumer (consumerKey, name, description, website, callbackUrl, secret, ownerId) values ('a08318eb478a1ee31f69a55276f3af64', 'Greenhouse for the iPhone', 'Awesome', 'http://www.springsource.com', 'x-com-springsource-greenhouse://oauth-response', '80e7f8f7ba724aae9103f297e5fb9bdf', 1);
+insert into OAuthToken (tokenValue, consumerKey, secret, callbackUrl, updateTimestamp, verifier, userId) values 
+                       ('requestToken', 'a08318eb478a1ee31f69a55276f3af64', 'tokenSecret', 'http://www.springsource.com', 0, 'verifier', 2);
