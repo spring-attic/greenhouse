c5815ef781c2cacff6b87a037793b08d31b0c428
habuma <cwalls@vmware.com>
2010-06-28 12:15:56 -0500

    Very initial pass at sending a Twitter status update upon linking with Twitter. It's hard-coded and doesn't use RestTemplate...but it's working and I want to commit before I screw that much up.

--- src/main/java/com/springsource/greenhouse/settings/TwitterController.java
+++ src/main/java/com/springsource/greenhouse/settings/TwitterController.java
@@ -1,10 +1,17 @@
 package com.springsource.greenhouse.settings;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth.consumer.OAuthConsumerSupport;
+import org.springframework.security.oauth.consumer.OAuthRequestFailedException;
+import org.springframework.security.oauth.consumer.token.OAuthConsumerToken;
+import org.springframework.security.oauth.consumer.token.OAuthConsumerTokenServices;
 import org.springframework.security.oauth.consumer.token.OAuthConsumerTokenServicesFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,13 +25,14 @@ import com.springsource.greenhouse.signin.GreenhouseUserDetails;
 public class TwitterController {
     
     private OAuthConsumerTokenServicesFactory tokenServicesFactory;
-
+    private OAuthConsumerSupport oauthSupport;
     private JdbcTemplate jdbcTemplate;
     
     @Inject
-    public TwitterController(OAuthConsumerTokenServicesFactory tokenServicesFactory, JdbcTemplate jdbcTemplate) {
+    public TwitterController(OAuthConsumerTokenServicesFactory tokenServicesFactory, OAuthConsumerSupport oauthSupport, JdbcTemplate jdbcTemplate) {
 		this.tokenServicesFactory = tokenServicesFactory;
 		this.jdbcTemplate = jdbcTemplate;
+		this.oauthSupport = oauthSupport;
 	}
 
 	@RequestMapping(value="/twitter", method=RequestMethod.GET)
@@ -40,7 +48,21 @@ public class TwitterController {
     public String authorize(HttpServletRequest request, Authentication authentication) {
         String oauthToken = request.getParameter("oauth_token");      
         if (oauthToken != null && oauthToken.length() > 0) {
-            FlashMap.getCurrent(request).put("connectedMessage", "Your Twitter account is now linked to your Greenhouse account!");
+
+            OAuthConsumerTokenServices tokenServices = tokenServicesFactory.getTokenServices(authentication, request);
+            OAuthConsumerToken accessToken = tokenServices.getToken("twitter");
+            
+            if(accessToken.isAccessToken()) {
+                try {
+                    oauthSupport.readProtectedResource(new URL("http://api.twitter.com/1/statuses/update.json?status=Did+this+work"), accessToken, "POST");
+                } catch (OAuthRequestFailedException e) {
+                    e.printStackTrace();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                
+                FlashMap.getCurrent(request).put("connectedMessage", "Your Twitter account is now linked to your Greenhouse account!");
+            }
         }
         return "redirect:/settings/twitter";
     }
