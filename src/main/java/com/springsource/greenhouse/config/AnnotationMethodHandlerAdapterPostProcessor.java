23f74699ad3f2b578dc405e256fc7f66dd3e0adb
Keith <keith@not-Scott-Andrewss-MacBook.local>
2010-06-28 02:06:31 -0400

    polish

--- src/main/java/com/springsource/greenhouse/config/AnnotationMethodHandlerAdapterPostProcessor.java
+++ src/main/java/com/springsource/greenhouse/config/AnnotationMethodHandlerAdapterPostProcessor.java
@@ -1,7 +1,5 @@
 package com.springsource.greenhouse.config;
 
-import javax.inject.Inject;
-
 import net.sourceforge.wurfl.core.Device;
 
 import org.springframework.beans.BeansException;
@@ -15,28 +13,23 @@ import org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAda
 
 // TODO - it would be better to do this as part of instantiating AnnotationMethodHandlerAdapter
 // TODO - support in Spring MVC namespace should be added for that (this would go away then)
+// TODO - see SPR-7327
 public class AnnotationMethodHandlerAdapterPostProcessor implements BeanPostProcessor {
-
-	private AnnotationMethodHandlerAdapter controllerInvoker;
-
-	@Inject
-	public AnnotationMethodHandlerAdapterPostProcessor(AnnotationMethodHandlerAdapter controllerInvoker) {
-		this.controllerInvoker = controllerInvoker;
-	}
 
 	public Object postProcessBeforeInitialization(Object bean, String name)
 			throws BeansException {
 		return bean;
 	}
 
-	public Object postProcessAfterInitialization(Object bean, String name)
-			throws BeansException {
-		controllerInvoker.setCustomArgumentResolver(new DeviceWebArgumentResolver());
+	public Object postProcessAfterInitialization(Object bean, String name) throws BeansException {
+		if (bean instanceof AnnotationMethodHandlerAdapter) {
+			AnnotationMethodHandlerAdapter controllerInvoker = (AnnotationMethodHandlerAdapter) bean;
+			controllerInvoker.setCustomArgumentResolver(new DeviceWebArgumentResolver());
+		}
 		return bean;
 	}
 
 	private static class DeviceWebArgumentResolver implements WebArgumentResolver {
-
 		public Object resolveArgument(MethodParameter param, NativeWebRequest request) throws Exception {
 			if (Device.class.isAssignableFrom(param.getParameterType())) {
 				return request.getAttribute(DeviceDetectingHandlerInterceptor.DEVICE_ATTRIBUTE, RequestAttributes.SCOPE_REQUEST);
@@ -44,7 +37,6 @@ public class AnnotationMethodHandlerAdapterPostProcessor implements BeanPostProc
 				return WebArgumentResolver.UNRESOLVED;
 			}
 		}
-
 	}
 
 }
