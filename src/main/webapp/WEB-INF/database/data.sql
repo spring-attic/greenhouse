e7da7cf7adaaeaeddd8b2be71679df7196dfdba2
Keith Donald <keith.donald@springsource.com>
2010-06-29 19:27:02 -0400

    temp change roy email

--- src/main/webapp/WEB-INF/database/data.sql
+++ src/main/webapp/WEB-INF/database/data.sql
@@ -1,5 +1,5 @@
 insert into User (firstName, lastName, email, password, username) values ('Keith' , 'Donald', 'kdonald@vmware.com', 'melbourne', 'kdonald');
 insert into User (firstName, lastName, email, password, username) values ('Craig' , 'Walls', 'cwalls@vmware.com', 'plano', 'habuma');
-insert into User (firstName, lastName, email, password, username) values ('Roy', 'Clarkson', 'rclarkson@vmware.com', 'atlanta', 'rclarkson');
+insert into User (firstName, lastName, email, password, username) values ('Roy', 'Clarkson', 'roy@clarkson.org', 'atlanta', 'rclarkson');
 
 insert into Consumer (consumerKey, name, description, website, callbackUrl, secret, ownerId) values ('a08318eb478a1ee31f69a55276f3af64', 'Greenhouse for the iPhone', 'Awesome', 'http://www.springsource.com', 'x-com-springsource-greenhouse://oauth-response', '80e7f8f7ba724aae9103f297e5fb9bdf', 2);
