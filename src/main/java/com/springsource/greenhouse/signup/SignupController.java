21fc0f43eb217e4ec891eb50923968cfbdecd5ea
habuma <cwalls@vmware.com>
2010-07-02 15:31:35 -0500

    Automatically login after registration.

--- src/main/java/com/springsource/greenhouse/signup/SignupController.java
+++ src/main/java/com/springsource/greenhouse/signup/SignupController.java
@@ -1,9 +1,17 @@
 package com.springsource.greenhouse.signup;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,33 +22,50 @@ import com.springsource.greenhouse.updates.GreenhouseUpdatesService;
 @Controller
 @RequestMapping("/signup")
 public class SignupController {
-	
+
 	private JdbcTemplate jdbcTemplate;
 	private GreenhouseUpdatesService updatesService;
-	
+	private AuthenticationManager authenticationManager;
+
 	@Inject
-	public SignupController(JdbcTemplate jdbcTemplate, GreenhouseUpdatesService updatesService) {
+	public SignupController(JdbcTemplate jdbcTemplate, GreenhouseUpdatesService updatesService, 
+			@Qualifier("org.springframework.security.authenticationManager") ProviderManager authenticationManager) {
 		this.jdbcTemplate = jdbcTemplate;
 		this.updatesService = updatesService;
+		this.authenticationManager = authenticationManager;
 	}
 
-	@RequestMapping(method=RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public SignupForm newForm() {
 		return new SignupForm();
 	}
 
-	@RequestMapping(method=RequestMethod.POST)
-	public String signup(@Valid SignupForm form, BindingResult result) {
+	@RequestMapping(method = RequestMethod.POST)
+	public String signup(@Valid SignupForm form, BindingResult result, HttpServletRequest request) {
 		if (result.hasErrors()) {
 			return null;
 		}
-		jdbcTemplate.update("insert into User (firstName, lastName, email, password) values (?, ?, ?, ?)", form.getFirstName(), form.getLastName(), form.getEmail(), form.getPassword());
-		
+		jdbcTemplate.update("insert into User (firstName, lastName, email, password) values (?, ?, ?, ?)", 
+				form.getFirstName(), form.getLastName(), form.getEmail(), form.getPassword());
+
 		// Add new Greenhouse Update
 		String updateText = form.getFirstName() + " " + form.getLastName() + " signed up at the Greenhouse.";
 		updatesService.createUpdate(updateText);
+
+		autoLogin(request, form.getEmail(), form.getPassword());
 		
-		return "redirect:/signin";
+		return "redirect:/";
+	}
+
+	public void autoLogin(HttpServletRequest request, String username, String password) {
+		try {
+			UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
+			token.setDetails(new WebAuthenticationDetails(request));
+			Authentication authentication = authenticationManager.authenticate(token);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		} catch (Exception e) {
+			SecurityContextHolder.getContext().setAuthentication(null);
+		}
 	}
 
 }
