bdf4d55d00004145926ceeaf0f71bd5576224c9e
habuma <cwalls@vmware.com>
2010-07-06 11:30:33 -0500

    Send email upon successful registration.

--- src/main/java/com/springsource/greenhouse/signup/SignupController.java
+++ src/main/java/com/springsource/greenhouse/signup/SignupController.java
@@ -19,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.springsource.greenhouse.signin.GreenhouseUserDetails;
 import com.springsource.greenhouse.signin.GreenhouseUserDetailsService;
+import com.springsource.greenhouse.signup.mail.SignupMessage;
+import com.springsource.greenhouse.signup.mail.SignupMessageGateway;
 import com.springsource.greenhouse.updates.GreenhouseUpdatesService;
 
 @Controller
@@ -29,15 +31,18 @@ public class SignupController {
 	private GreenhouseUpdatesService updatesService;
 	private GreenhouseUserDetailsService userDetailsService;
 	private AuthenticationManager authenticationManager;
+	private SignupMessageGateway signupMessageGateway;
 
 	@Inject
 	public SignupController(JdbcTemplate jdbcTemplate, GreenhouseUpdatesService updatesService, 
 			GreenhouseUserDetailsService userDetailsService,
-			@Qualifier("org.springframework.security.authenticationManager") ProviderManager authenticationManager) {
+			@Qualifier("org.springframework.security.authenticationManager") ProviderManager authenticationManager,
+			SignupMessageGateway signupMessageGateway) {
 		this.jdbcTemplate = jdbcTemplate;
 		this.updatesService = updatesService;
 		this.userDetailsService = userDetailsService;
 		this.authenticationManager = authenticationManager;
+		this.signupMessageGateway = signupMessageGateway;
 	}
 
 	@RequestMapping(method = RequestMethod.GET)
@@ -53,7 +58,11 @@ public class SignupController {
 		jdbcTemplate.update("insert into User (firstName, lastName, email, password) values (?, ?, ?, ?)", 
 				form.getFirstName(), form.getLastName(), form.getEmail(), form.getPassword());
 		
+		int userId = jdbcTemplate.queryForInt("select id from User where email = ?", form.getEmail());
+		
 		postUpdate(form.getFirstName(), form.getLastName(), form.getEmail());
+
+		signupMessageGateway.publish(new SignupMessage(userId, form.getEmail(), form.getFirstName()));
 		
 		loginAfterSignup(request, form.getEmail(), form.getPassword());
 		
