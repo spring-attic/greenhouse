18c3a66fafa1d8b251ea6c330f25b9517aa0bddc
Keith <keith@not-Scott-Andrewss-MacBook.local>
2010-07-07 03:00:27 -0400

    polish

--- src/main/java/com/springsource/greenhouse/updates/UpdatesController.java
+++ src/main/java/com/springsource/greenhouse/updates/UpdatesController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
