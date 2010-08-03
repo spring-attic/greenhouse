3ac3348b78ae473a8c8c6c5ea2bd1d4b507a4928
Keith Donald <keith.donald@springsource.com>
2010-08-03 01:13:49 -0400

    polish

--- src/main/webapp/WEB-INF/database/data.sql
+++ src/main/webapp/WEB-INF/database/data.sql
@@ -23,21 +23,21 @@ insert into EventTrack (event, code, name, description, chair) values (1, 'spr',
 insert into EventTrack (event, code, name, description, chair) values (1, 'web', 'Web Application Development', 'What you need to know to build rich web applications', 1);
 
 insert into EventSession (event, number, title, startTime, endTime, description, hashtag, track) values (1, 1, 'Mastering MVC 3', '2010-10-20 16:45:00Z', '2010-10-20 18:15:00Z',
-	'A deep-dive into the latest capabilities of Spring MVC, Spring''s REST-ful web application development platform.', '#web101', 'web');
+	'A deep-dive into the latest capabilities of Spring MVC, Spring''s REST-ful web application development platform.', '#mvc', 'web');
 insert into EventSessionLeader (event, session, leader) values (1, 1, 1);
 
 insert into EventSession (event, number, title, startTime, endTime, description, hashtag, track) values (1, 2, 'Inside Web Flow 3 Development', '2010-10-20 18:45:00Z', '2010-10-20 20:15:00Z',
-	'A look inside the development of Spring Web Flow 3, the next-generation version of Spring''s stateful controller framework for orchestrating multi-step user dialogs.', '#web102', 'web');
+	'A look inside the development of Spring Web Flow 3, the next-generation version of Spring''s stateful controller framework for orchestrating multi-step user dialogs.', '#webflow', 'web');
 insert into EventSessionLeader (event, session, leader) values (1, 2, 1);
 
 insert into EventSession (event, number, title, startTime, endTime, description, hashtag, track) values (1, 3, 'Developing Social-Ready Web Applications', '2010-10-21 16:45:00Z', '2010-10-21 18:15:00Z',
 	'Businesses are increasingly recognizing the value of connecting with their customers on a more personal level. Companies can utilize social networking to transition from "Big Faceless Corporation" to "Friend" by taking their wares to the online communities where their customers are. In this age of social media, those communities are found at social network sites such as Facebook, Twitter, and LinkedIn. In this session, you''ll learn how to build Spring-based applications that interact with the various social networks. We''ll talk about new features in the Spring portfolio to support integration with social networks as well as how to start building social features into your own applications.',
-	'#web201', 'web');
+	'#social', 'web');
 insert into EventSessionLeader (event, session, leader) values (1, 3, 2);
 	
 insert into EventSession (event, number, title, startTime, endTime, description, hashtag, track) values (1, 4, 'Choices in Moble Application Development', '2010-10-21 18:45:00Z', '2010-10-21 20:15:00Z',
 	'With the rising prevalence of advanced mobile platforms such as iPhone, Android, and Web OS, the desire for rich mobile clients as another means of accessing enterprise services is becoming something that can no longer be ignored. In this session, we will explore the current mobile development landscape and discuss what you as a Spring developer can do to support this increasingly important paradigm. We will examine the benefits and tradeoffs of native mobile client development vs. web-based mobile client development, and we will explore some of the emerging cross-platform options such as PhoneGap. We will look at the various strategies for utilizing a Spring back-end with these mobile platforms, such as consumption of RESTful services, authentication and authorization via OAuth, and server-push style messaging.',
-	'#web202', 'web');
+	'#mobile', 'web');
 insert into EventSessionLeader (event, session, leader) values (1, 4, 3);
 insert into EventSessionLeader (event, session, leader) values (1, 4, 4);
 
