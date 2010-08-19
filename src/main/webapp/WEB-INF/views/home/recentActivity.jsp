e2db5ba294932f5615f77eb83621069f353c9d98
Keith Donald <keith.donald@springsource.com>
2010-08-19 17:34:03 -0400

    polish

--- src/main/webapp/WEB-INF/views/home/recentActivity.jsp
+++ src/main/webapp/WEB-INF/views/home/recentActivity.jsp
@@ -1,9 +1,11 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<ul id="recentActivityList">
+<ul id="recentActivity">
 	<c:forEach var="item" items="${recentActivity}">
 		<li>
 			<img class="profile" src="${item.memberPictureUrl}" />
-			<c:out value="${item.text}" escapeXml="true" />
+			<p>
+				<c:out value="${item.text}" escapeXml="true" />
+			</p>
 			<img class="badge" src="${item.imageUrl}" />
 		</li>
 	</c:forEach>
--- src/main/webapp/resources/recentActivity.js
+++ src/main/webapp/resources/recentActivity.js
@@ -7,23 +7,23 @@ $(document).ready(function (){
 		
 		$("<li></li>").
 	    	addClass("newItem").
-	    	append($("<img/>", {"src":(activity.memberPictureUrl ? activity.memberPictureUrl : "")}).addClass("profile")).
+	    	append($("<img/>", { "src" : activity.memberPictureUrl }).addClass("profile")).
 	    	append(activity.text).
-	    	append($("<img/>", {"src":(activity.imageUrl ? activity.imageUrl : "")}).addClass("badge")).
-	    	prependTo("#recentActivityList");
+	    	append($("<img/>", { "src" : activity.imageUrl }).addClass("activity")).
+	    	prependTo("#recentActivity");
 		
-		if ($("#recentActivityList li").length == 6) {
-		    $("#recentActivityList li:last").slideUp("slow", function() {
+		if ($("#recentActivity li").length == 6) {
+		    $("#recentActivity li:last").slideUp("slow", function() {
 		        $(this).remove();
 		    });
 		}
-		$("#recentActivityList li.newItem").slideDown("slow", function() {
+		$("#recentActivity li.newItem").slideDown("slow", function() {
 		    $(this).removeClass("newItem");
 		});
 	}
 	
 	/* transport can be : long-polling, streaming or websocket */
 	$.atmosphere.subscribe(basePath + '/pubsub/recent/notifications', activityNotificationHandler,
-            $.atmosphere.request = {transport: "streaming", headers: {'Accept' : "application/json"}});
+            $.atmosphere.request = { transport: "streaming", headers: { "Accept" : "application/json" } });
 	
 });
