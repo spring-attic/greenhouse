c2b3fb948dbd26ee51e2308bbe22222b718c401e
Keith Donald <keith.donald@springsource.com>
2010-09-01 13:39:12 -0400

    switched to embedded mode and h2's own pool

--- src/main/java/com/springsource/greenhouse/database/GreenhouseConnectionPoolFactoryBean.java
+++ src/main/java/com/springsource/greenhouse/database/GreenhouseConnectionPoolFactoryBean.java
@@ -2,36 +2,22 @@ package com.springsource.greenhouse.database;
 
 import javax.sql.DataSource;
 
+import org.h2.jdbcx.JdbcConnectionPool;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Value;
-
-import com.jolbox.bonecp.BoneCPDataSource;
 
 public class GreenhouseConnectionPoolFactoryBean implements FactoryBean<DataSource>, InitializingBean, DisposableBean {
 
-	private BoneCPDataSource connectionPool;
+	private JdbcConnectionPool connectionPool;
 	
-	@Value("${database.url}")
-	private String url;
-	
-	@Value("${database.username}")
-	private String username;
-
-	@Value("${database.password}")
-	private String password;
-
 	public void afterPropertiesSet() throws Exception {
-		connectionPool = new BoneCPDataSource();
-		connectionPool.setJdbcUrl(url);
-		connectionPool.setUsername(username);
-		connectionPool.setPassword(password);
+		connectionPool = JdbcConnectionPool.create("jdbc:h2:~/greenhouse", "sa", "sa");
 		populateDatabase();
 	}
 	
 	public void destroy() throws Exception {
-		connectionPool.close();
+		connectionPool.dispose();
 	}
 
 	public DataSource getObject() throws Exception {
