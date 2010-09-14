11c0cbcde2c6482da6c190bb4cc855c4982df182
Keith Donald <keith.donald@springsource.com>
2010-09-14 00:56:43 -0400

    Added generator test case

---
+++ src/test/java/org/springframework/security/encrypt/SecureRandomKeyGeneratorTests.java
@@ -0,0 +1,21 @@
+package org.springframework.security.encrypt;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class SecureRandomKeyGeneratorTests {
+	
+	private SecureRandomKeyGenerator generator = new SecureRandomKeyGenerator();
+	
+	@Test
+	public void generateKey() {
+		byte[] bytes = generator.generateKey();
+		System.out.println(EncodingUtils.hexEncode(bytes));
+		assertEquals(8, bytes.length);
+		assertFalse(Arrays.equals(bytes, generator.generateKey()));
+	}
+}
