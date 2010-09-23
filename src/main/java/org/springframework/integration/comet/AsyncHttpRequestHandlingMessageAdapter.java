23a2e20bc11a1fe16b047815b00b79686a62f8bf
Keith Donald <keith.donald@springsource.com>
2010-09-23 12:17:17 -0400

    polishing

--- src/main/java/org/springframework/integration/comet/AsyncHttpRequestHandlingMessageAdapter.java
+++ src/main/java/org/springframework/integration/comet/AsyncHttpRequestHandlingMessageAdapter.java
@@ -3,7 +3,6 @@ package org.springframework.integration.comet;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
