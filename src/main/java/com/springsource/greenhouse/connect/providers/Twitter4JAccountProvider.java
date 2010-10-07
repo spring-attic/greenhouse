1bdc778e2c09aaabee7a4b7cfb983ae35129ef4b
Craig Walls <cwalls@vmware.com>
2010-10-07 10:27:23 -0500

    Added Twitter4JAccountProvider

---
+++ src/main/java/com/springsource/greenhouse/connect/providers/Twitter4JAccountProvider.java
@@ -0,0 +1,50 @@
+package com.springsource.greenhouse.connect.providers;
+
+import java.util.Properties;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.PropertyConfiguration;
+import twitter4j.http.AccessToken;
+import twitter4j.http.Authorization;
+import twitter4j.http.OAuthAuthorization;
+
+import com.springsource.greenhouse.connect.AbstractAccountProvider;
+import com.springsource.greenhouse.connect.AccountConnectionRepository;
+import com.springsource.greenhouse.connect.AccountProviderParameters;
+import com.springsource.greenhouse.connect.OAuthToken;
+
+public class Twitter4JAccountProvider extends AbstractAccountProvider<Twitter> {
+	public Twitter4JAccountProvider(AccountProviderParameters parameters,
+			AccountConnectionRepository connectionRepository) {
+		super(parameters, connectionRepository);
+	}
+
+	public Twitter createApi(OAuthToken accessToken) {
+		TwitterFactory twitterFactory = new TwitterFactory();
+
+		AccessToken oauthToken = new AccessToken(accessToken.getValue(), accessToken.getSecret());
+		Configuration configuration = new PropertyConfiguration(new Properties());
+		Authorization authorization = new OAuthAuthorization(configuration, getApiKey(), getSecret(), oauthToken);
+
+		return accessToken != null ? twitterFactory.getInstance(authorization) : twitterFactory.getInstance();
+	}
+
+	public String getProviderAccountId(Twitter api) {
+		try {
+			return api.getScreenName();
+		} catch (TwitterException e) {
+			return null;
+		}
+	}
+
+	public String getProviderProfileUrl(Twitter api) {
+		try {
+			return "http://www.twitter.com/" + api.getScreenName();
+		} catch (TwitterException e) {
+			return null;
+		}
+	}
+}
