ceaa80d1ac0f1f0067d928d3d0aca8c6185244e2
Keith <keith@not-Scott-Andrewss-MacBook.local>
2010-10-21 17:21:04 -0400

    polish

--- src/main/java/com/springsource/greenhouse/events/SessionNotEndedException.java
+++ src/main/java/com/springsource/greenhouse/events/SessionNotEndedException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @SuppressWarnings("serial")
-@ResponseStatus(value=HttpStatus.PRECONDITION_FAILED)
+@ResponseStatus(HttpStatus.PRECONDITION_FAILED)
 public final class SessionNotEndedException extends Exception {
 	
 	private final Long eventId;
