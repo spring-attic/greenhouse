72ccb671b22b6c0ade758d59620fc323dbce235d
Keith Donald <keith.donald@springsource.com>
2011-02-19 02:03:16 -0500

    plugged in text encryptor

--- src/main/java/com/springsource/greenhouse/config/connect/ConnectionRepositoryConfig.java
+++ src/main/java/com/springsource/greenhouse/config/connect/ConnectionRepositoryConfig.java
@@ -19,7 +19,7 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.security.crypto.encrypt.TextEncryptor;
 import org.springframework.social.connect.jdbc.JdbcConnectionRepository;
 import org.springframework.social.connect.support.ConnectionRepository;
 
@@ -27,8 +27,8 @@ import org.springframework.social.connect.support.ConnectionRepository;
 public class ConnectionRepositoryConfig {
 
 	@Bean
-	public ConnectionRepository connectionRepository(DataSource dataSource) {
-		return new JdbcConnectionRepository(dataSource, Encryptors.noOpText());
+	public ConnectionRepository connectionRepository(DataSource dataSource, TextEncryptor encryptor) {
+		return new JdbcConnectionRepository(dataSource, encryptor);
 	}
 	
 }
