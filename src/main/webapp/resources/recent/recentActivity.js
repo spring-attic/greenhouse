b128c003ed9ddc153841d3ec6a193905f6d6b416
Jeremy Grelle <jgrelle@vmware.com>
2011-05-02 16:10:52 -0700

    Updates for using the new greenhouse-notifications NodeJS app for Comet/WebSocket notifications.

--- src/main/webapp/resources/recent/recentActivity.js
+++ src/main/webapp/resources/recent/recentActivity.js
@@ -1,23 +1,47 @@
-$(document).ready(function() {
-	var basePath = location.protocol + "//" + location.host + application.contextPath;
-	
-	function activityNotificationHandler(response) {
-		var activityItems = $.parseJSON(response.responseBody);
-		for (var i = 0; i < activityItems.length; i++) {
-			var activity = activityItems[i];
-			$("<li class='listing'></li>").addClass("newItem").append($("<img/>", { "src" : activity.memberPictureUrl }).addClass("profile")).
-				append("<p>" + activity.text + "</p>").
-				append($("<img/>", { "src" : activity.imageUrl}).addClass("activity")).
-				prependTo("#recentActivityList");	
-			if ($("#recentActivityList li").length == 6) {
-				$("#recentActivityList li:last").slideUp("slow", function() {
-					$(this).remove();
-				});
-			}
-			$("#recentActivityList li.newItem").slideDown("slow", function() {
-				$(this).removeClass("newItem");
+var canLog = window.console && console.log;
+
+function activityNotificationHandler(activity) {
+		$("<li class='listing'></li>").addClass("newItem").append($("<img/>", { "src" : activity.memberPictureUrl }).addClass("profile")).
+			append("<p>" + activity.text + "</p>").
+			append($("<img/>", { "src" : activity.imageUrl}).addClass("activity")).
+			prependTo("#recentActivityList");	
+		if ($("#recentActivityList li").length == 6) {
+			$("#recentActivityList li:last").slideUp("slow", function() {
+				$(this).remove();
 			});
 		}
-	}
+		$("#recentActivityList li.newItem").slideDown("slow", function() {
+			$(this).removeClass("newItem");
+		});
+}
 
+function log(message) {
+	if (canLog) {
+		console.log(message);
+	}
+}
+
+function error(message) {
+	if (canLog) {
+		console.error(message);
+	}
+}
+
+var socket = new io.Socket(!location.hostname.match(/localhost/) ? 'greenhouse-notifications.cloudfoundry.com' : location.hostname, 
+	{ port: document.location.port == 8080 ? 8088 : document.location.port, 
+	  rememberTransport:false, 
+	  transports: ['xhr-polling'] }
+);
+
+$(document).ready(function() {
+    setTimeout(function () {
+        socket.connect();
+        socket.on('message', activityNotificationHandler);
+
+        socket.on('connect',  function(){ log('Connected'); });
+        socket.on('disconnect', function(){ error('Disconnected'); });
+        socket.on('reconnect', function(){ log('Reconnected to server'); });
+        socket.on('reconnecting', function(){ log('Attempting to re-connect to the server, next attempt in ' + nextRetry + 'ms'); });
+        socket.on('reconnect_failed', function(){ error('Reconnected to server FAILED.'); });
+    }, 1000);
 });
