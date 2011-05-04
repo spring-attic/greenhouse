2e3082c13e32951392168a7c56ba8faa73245ab5
Keith Donald <keith.donald@springsource.com>
2011-05-04 11:19:07 -0400

    added profile to exclude redis in embedded environment

--- src/main/java/com/springsource/greenhouse/config/activity/RedisIntegrationConfig.java
+++ src/main/java/com/springsource/greenhouse/config/activity/RedisIntegrationConfig.java
@@ -6,6 +6,7 @@ import org.cloudfoundry.runtime.env.CloudEnvironment;
 import org.cloudfoundry.runtime.service.keyvalue.RedisServiceCreator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.keyvalue.redis.connection.RedisConnectionFactory;
 import org.springframework.data.keyvalue.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.integration.json.JsonInboundMessageMapper;
@@ -15,9 +16,11 @@ import org.springframework.integration.support.converter.SimpleMessageConverter;
 import org.springframework.util.CollectionUtils;
 
 @Configuration
+@Profile("standard")
 public class RedisIntegrationConfig {
 
-	@Bean public RedisConnectionFactory redisConnectionFactory() {
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
 		if (CollectionUtils.isEmpty(cloudEnvironment().getServices())) {
 			return new JedisConnectionFactory();
 		} else {
@@ -25,15 +28,18 @@ public class RedisIntegrationConfig {
 		}
 	}
 	
-	@Bean CloudEnvironment cloudEnvironment() {
+	@Bean
+	public CloudEnvironment cloudEnvironment() {
 		return new CloudEnvironment();
 	}
 	
-	@Bean RedisServiceCreator redisServiceCreator() {
+	@Bean
+	public RedisServiceCreator redisServiceCreator() {
 		return new RedisServiceCreator(cloudEnvironment());
 	}
 	
-	@Bean MessageConverter jsonConverter() {
+	@Bean
+	public MessageConverter jsonConverter() {
 		SimpleMessageConverter converter = new SimpleMessageConverter();
 		JsonInboundMessageMapper inbound = new JsonInboundMessageMapper(HashMap.class);
 		inbound.setMapToPayload(true);
